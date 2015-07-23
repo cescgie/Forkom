b4d94ba76c8a03b6794504357545660a82b9191c
jeins <mail@mjuanakbar.info>
2015-07-23 22:51:21 +0200

    add responsive tab

---
+++ asset/js/setup-tabs.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var $tabs = $('#horizontalTab');
+    $tabs.responsiveTabs({
+        rotate: false,
+        startCollapsed: 'accordion',
+        collapsible: 'accordion',
+        setHash: true,
+        activate: function(e, tab) {
+            $('.info').html('Tab <strong>' + tab.id + '</strong> activated!');
+        },
+        activateState: function(e, state) {
+            //console.log(state);
+            $('.info').html('Switched from <strong>' + state.oldState + '</strong> state to <strong>' + state.newState + '</strong> state!');
+        }
+    });
+});
